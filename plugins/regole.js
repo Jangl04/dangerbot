b9e06c17f754defaa7def26bd89dd232dce67e83
Jangl04 <giuliocaroli25@gmail.com>
2026-02-09 13:09:37 +0100

    Update regole.js

--- plugins/regole.js
+++ plugins/regole.js
@@ -2,12 +2,13 @@ let handler = async (m, { conn }) => {
   // ✏️ MODIFICA QUI LE REGOLE
   const regole =
 `📌 *REGOLE DEL GRUPPO*
+
 1) Si al flame.
 2) Niente spam / flood.
 3) Niente link a caso senza permesso.
 4) Niente porno / gore .
 5) Non scrivere in privato senza permesso.
-6) Se hai problemi, tagga un admin: *.admin*
+6) Se hai problemi, tagga un admin
 
 ✅ Chi non rispetta le regole può essere mutato o rimosso.`
 
