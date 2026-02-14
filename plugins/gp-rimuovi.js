1965e00c7e11cfa9134e77ecf91f6c802f2ec0a3
Jangl04 <giuliocaroli25@gmail.com>
2026-02-14 01:23:31 +0100

    Update gp-rimuovi.js

--- plugins/gp-rimuovi.js
+++ plugins/gp-rimuovi.js
@@ -72,7 +72,7 @@ var handler = async (m, { conn, participants }) => {
 }
 handler.help = ['rimuovi']
 handler.tags = ['gruppo']
-handler.command = /^(kick|rimuovi|paki|ban)$/i
+handler.command = /^(kick|cacca|sparisci|avadachedavra|ban)$/i
 handler.group = true
 handler.admin = true
 handler.botAdmin = true
