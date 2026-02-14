55ce559de8023eefe767540ac39a1dc36ac8e201
Jangl04 <giuliocaroli25@gmail.com>
2026-02-14 22:06:30 +0100

    Update giochi-sposa.js

--- plugins/giochi-sposa.js
+++ plugins/giochi-sposa.js
@@ -544,5 +544,4 @@ handler.help = ['sposa @tag', 'divorzia'];
 handler.tags = ['giochi'];
 handler.command = /^(sposa|marry|divorzia)$/i;
 handler.group = true;
-handler.register = true;
 export default handler;
