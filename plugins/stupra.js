7c6bbb174924b5da635913328727322a5b72e74a
Jangl04 <giuliocaroli25@gmail.com>
2026-02-14 13:28:56 +0100

    Create stupra.js

---
+++ plugins/stupra.js
@@ -0,0 +1,24 @@
+function pick(arr) {
+  return arr[Math.floor(Math.random() * arr.length)]
+}
+
+const frasi = [
+  "⁨sei stat𝐚 stuprat𝐚 a 90 e ti hanno trattato come una puttana di merda “AHHH…AAAHHH, si continua non fermarti e ti ha stuprato così violentemente che non riesci nemmeno a reggerti in piedi stupida troia di merda" 
+]
+
+let handler = async (m, { conn }) => {
+
+  const frase = pick(frasi)
+
+  return conn.reply(
+    m.chat,
+    `👑 *Domina Mode Attivato* 👑\n\n${frase}`,
+    m
+  )
+}
+
+handler.help = ['stupra']
+handler.tags = ['fun']
+handler.command = ['stupra']
+
+export default handler
