57238cc9ec184a3e80c51926e6d459b212410294
dthbot <blebledarius@gmail.com>
2026-02-19 19:33:30 +0100

    Update creatore-sbanchat.js

--- plugins/creatore-sbanchat.js
+++ plugins/creatore-sbanchat.js
@@ -1,16 +1,22 @@
-let handler = async (m, { conn }) => {
-if (!(m.chat in global.db.data.chats)) return conn.reply(m.chat, '🎌 *Questo chat non è registrata!*', m, fake)
-let chat = global.db.data.chats[m.chat]
-if (!chat.isBanned) return conn.reply(m.chat, '《★》Il bot non è bannato in questa chat', m, fake)
-chat.isBanned = false
-await conn.reply(m.chat, `《★》varebot è stato sbannato in questa chat.`, m, fake)
-}
-handler.help = ['sbanchat'];
-handler.tags = ['creatore'];
-handler.command = ['unbanchat', 'sbannachat', 'sbanchat']
-handler.rowner = true
-handler.admin = true 
-handler.botAdmin = false
-handler.group = false
+let handler = async (m) => {
+  global.db.data.chats[m.chat].isBanned = false;
+  let message = '𝔹𝕌𝕆ℕ𝔾𝕀𝕆ℝℕ𝕆 𝕄𝔼ℝ𝔻𝔼, 𝕄𝕀 𝕊𝕆ℕ𝕆 𝕊𝕍𝔼𝔾𝕃𝕀𝔸𝕋𝕆 🫨';
+  await conn.sendMessage(m.chat, { 
+      text: message,
+      contextInfo: {
+          forwardingScore: 99,
+          isForwarded: true,
+          forwardedNewsletterMessageInfo: {
+              newsletterJid: '',
+              serverMessageId: '',
+              newsletterName: 'THE-DANGER'
+          }
+      }
+  }, { quoted: m });
+};
 
-export default handler
+handler.help = ['unbanchat'];
+handler.tags = ['owner'];
+handler.command = /^on$/i;
+handler.rowner = true;
+export default handler;
