adabd0f3f6faa46920735f291ffd7be1e24d3452
Jangl04 <giuliocaroli25@gmail.com>
2026-02-22 22:52:14 +0100

    Create sega.js

---
+++ plugins/sega.js
@@ -0,0 +1,19 @@
+  let handler = async (m, { conn, usedPrefix, text }) => {
+  let { key } = await conn.sendMessage(m.chat, { text: "ah, quindi la mettiamo cosi?" }, { quoted: m });
+  const array = [
+    "8==👊==D", "8===👊=D", "8=👊===D", "8==👊==D", "8===👊=D", "8====👊D", "8===👊=D", "8==👊==D", "8=👊===D", "8👊====D", "8=👊===D","8==👊==D", "8===👊=D", "8====👊D","8==👊==D", "8===👊=D", "8=👊===D", "8=👊===D", "8==👊==D", "8===👊=D", "8====👊D💦"
+  ];
+
+  for (let item of array) {
+    await conn.sendMessage(m.chat, { text: `${item}`, edit: key }, { quoted: m });
+    await new Promise(resolve => setTimeout(resolve, 200)); // Delay di 2 secondi per prevenirlo ma nulla
+  }
+  return conn.sendMessage(m.chat, { text: `Oh, finalmente mi hai fatto venire  💦`.trim() , edit: key, mentions: [m.sender] }, { quoted: m });
+};
+
+handler.help = ['sega'];
+handler.tags = ['giochi'];
+handler.command = /^sega$/i;
+handler.register = true;
+handler.disabled = true;//rate overlimit alle porte
+export default handler;
