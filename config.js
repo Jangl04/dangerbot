5351b436b30956d18a1898e94b1fc98526254014
Jangl04 <giuliocaroli25@gmail.com>
2026-02-22 02:27:53 +0100

    Update config.js

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ global.luxifer = ['212781816909',]
 global.owner = [
   ['212781816909', 'luxifer', true],
   ['16093013226', 'mrciao', true], 
-  ['639753512076' 'anna', true], 
+  ['639753512076', 'anna', true], 
 ]
 global.mods = ['212781816909', '390935931875']
 global.prems = ['212781816909', '390935931875']
