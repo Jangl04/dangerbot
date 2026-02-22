05075235a6ba487fc9d9a7d46ecddbfa09d55555
Jangl04 <giuliocaroli25@gmail.com>
2026-02-22 22:58:21 +0100

    Update create sega.js

--- plugins/create sega.js
+++ plugins/create sega.js
@@ -1,12 +1,12 @@
   let handler = async (m, { conn, usedPrefix, text }) => {
   let { key } = await conn.sendMessage(m.chat, { text: "ah, quindi la mettiamo cosi?" }, { quoted: m });
   const array = [
-    "8==👊==D", "8===👊=D", "8=👊===D", "8==👊==D", "8===👊=D", "8====👊D", "8===👊=D", "8==👊==D", "8=👊===D", "8👊====D", "8=👊===D","8==👊==D", "8===👊=D", "8====👊D","8==👊==D", "8===👊=D", "8=👊===D", "8=👊===D", "8==👊==D", "8===👊=D", "8====👊D💦"
+    "8==👊==D", "8===👊=D", "8=👊===D", "8==👊==D", "8===👊=D", "8====👊D", "8===👊=D", "8==👊==D", "8=👊===D", "8👊====D", "8=👊===D","8==👊==D", "8===👊=D", "8====👊D","8==👊==D", "8===👊=D💦"
   ];
 
   for (let item of array) {
     await conn.sendMessage(m.chat, { text: `${item}`, edit: key }, { quoted: m });
-    await new Promise(resolve => setTimeout(resolve, 100)); // Delay di 1 secondi per prevenirlo ma nulla
+    await new Promise(resolve => setTimeout(resolve, 50)); // Delay di 1 secondi per prevenirlo ma nulla
   }
   return conn.sendMessage(m.chat, { text: `Oh, finalmente mi hai fatto venire  💦`.trim() , edit: key, mentions: [m.sender] }, { quoted: m });
 };
